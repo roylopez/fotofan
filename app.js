0f6815f6768dba1b31e980027efdeac8c090d2b9
ROY <roy.lalo@hotmail.com>
2016-12-31 15:44:38 -0500

    hola

--- app.js
+++ app.js
@@ -106,11 +106,4 @@ app.use("/app",sessionMiddleware);
 app.use("/app",routerApp);
 
 //app.listen(8000); Este cambio es para socket.io
-//server.listen(8000);
-
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
-
-server.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", port " + server_port )
-});
+server.listen(8000);
